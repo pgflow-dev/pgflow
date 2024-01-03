7950e493951df3e5ba0a864aed805d88954e1a2c
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-03 10:10:28 +0100

    generate supabase types and use it to create client

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_PROJECT_ID, SUPABASE_ANON_KEY } from '$env/static/private';
+import type { Database } from '../../../supabase/types/supabase';
 
-export const supabase = createClient(`https://${SUPABASE_PROJECT_ID}.supabase.co`, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(`https://${SUPABASE_PROJECT_ID}.supabase.co`, SUPABASE_ANON_KEY);
