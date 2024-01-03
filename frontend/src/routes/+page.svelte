1a7f1a1b6393c9d134ccbcae915272ef135a1231
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-03 05:40:08 +0100

    show table from dosc in root page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,25 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+import type { TableSource } from '@skeletonlabs/skeleton';
+
+const sourceData = [
+	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+];
+
+const tableSimple: TableSource = {
+	head: ['Name', 'Symbol', 'Weight'],
+	body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+	meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+	foot: ['Total', '', '<code class="code">5</code>']
+};
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+		<Table source={tableSimple} />
 	</div>
 </div>
