6c97eff2a4dc1e5533e160710fa15c6a11a7ffc0
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-08 00:44:09 +0100

    frontend: rename PUBLIC_SUPABASE_PROJECT_ID to *_URL

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_PROJECT_ID, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import type { Database } from '$backend/types';
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_PROJECT_ID, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
