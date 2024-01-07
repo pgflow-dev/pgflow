01e7bc428c446e67b62dd8226fc311b7ac200a26
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-07 23:23:44 +0100

    remove old migrations

--- supabase/migrations/20240103215448_create_documents.sql
+++
@@ -1,8 +0,0 @@
--- postgres table 'documents' with columns:
--- uid uuid, created_at timestampwithtz, content text, embeddings vector(768)
-create table documents (
-  uid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  created_at timestamp with time zone NOT NULL DEFAULT now(),
-  content text NOT NULL,
-  embedding vector(768)
-);
