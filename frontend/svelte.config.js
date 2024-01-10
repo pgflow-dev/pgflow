364522c994122ed1a9edbfd148dd53f6c9b1de11
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-10 20:11:05 +0100

    prettier --write . (spaces=>tabs mostly)

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,23 +5,23 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte'],
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess()],
+	extensions: ['.svelte'],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess()],
 
-  vitePlugin: {
-    inspector: true,
-  },
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-    alias: {
-      // custom alias for supabase backend
-      $backend: `${path.dirname(fileURLToPath(import.meta.url))}/../supabase`
-    }
-  }
+	vitePlugin: {
+		inspector: true
+	},
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			// custom alias for supabase backend
+			$backend: `${path.dirname(fileURLToPath(import.meta.url))}/../supabase`
+		}
+	}
 };
 export default config;
