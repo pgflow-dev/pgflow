e24f81b87eb5872d8e6072cfc19a0d5ef82cebb3
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-11 19:46:26 +0100

    frontend: setup @supabase/ssr client

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -2,7 +2,10 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		supabase: SupabaseClient;
+		getSession(): Promise<Session | null>;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
