2126f70baf6af3242b701591cd56c42cf7202301
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-12 23:15:20 +0100

    add badge

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -27,18 +27,18 @@
 			bind:value={currentMessage}
 			bind:inProgress
 			on:click={checkSentiment}
-			placeholder="Enter a message"
+			placeholder="Enter a message to check sentiment"
 		/>
 	</div>
 
 	<div class="w-1/3 flex items-center justify-center">
 		{#if sentiment}
 			{#if sentiment === 'POSITIVE'}
-				<span class="text-success-500">POSITIVE</span>
+				<span class="badge variant-filled-success">POSITIVE</span>
 			{:else if sentiment === 'NEGATIVE'}
-				<span class="text-error-500">NEGATIVE</span>
+				<span class="badge variant-filled-error">NEGATIVE</span>
 			{:else}
-				<span class="text-warning-500">NEUTRAL</span>
+				<span class="badge variant-filled-secondary">NEUTRAL</span>
 			{/if}
 		{/if}
 	</div>
