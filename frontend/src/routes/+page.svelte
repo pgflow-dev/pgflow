46ff1c41e704bddcdfc19618352f8c1e8eb35cf3
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-12 01:04:26 +0100

    finally a simple centered input

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let currentMessage = '';
+</script>
+
+<div class="flex h-screen">
+	<div class="w-1/3"></div>
+	<div class="w-1/3 flex items-center justify-center">
+		<input class="input" type="text" placeholder="what is on your mind?" />
+	</div>
+	<div class="w-1/3"></div>
+</div>
