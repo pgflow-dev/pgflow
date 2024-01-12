6ad4d932af9807a8bb12307ed7e8d7c013a337cc
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-13 00:13:39 +0100

    Focus input after submitting

--- frontend/src/routes/SubmittableInput.svelte
+++ frontend/src/routes/SubmittableInput.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, tick } from 'svelte';
 	const dispatch = createEventDispatcher();
 
 	export let value: string;
 	export let placeholder: string;
 	export let inProgress: boolean = false;
+
+	let input: HTMLInputElement;
+
+	async function focusInput() {
+		await tick();
+		input.focus();
+	}
+
+	$: !inProgress && focusInput();
 </script>
 
 <form class="form" on:submit|preventDefault={() => inProgress || dispatch('submit')}>
-	<input bind:value disabled={inProgress} class="input" type="text" {placeholder} />
+	<input
+		bind:value
+		bind:this={input}
+		disabled={inProgress}
+		class="input"
+		type="text"
+		{placeholder}
+	/>
 </form>
 
 {#if inProgress}
