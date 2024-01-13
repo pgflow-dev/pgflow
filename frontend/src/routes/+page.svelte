3e9984ea824f7c32363dd38e55ec390ce1aaf486
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-13 02:45:20 +0100

    move sentiment to /check-sentiment, add clock on root page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,73 +1,30 @@
 <script lang="ts">
-	import Prompt from '$components/Prompt.svelte';
+	import { onMount, onDestroy } from 'svelte';
 
-	export let data;
-	let { supabase } = data;
-	$: ({ supabase } = data);
+	let currentDate = new Date();
+	let interval: ReturnType<typeof setInterval>;
 
-	let currentMessage = '';
-	let inProgress = false;
-	let sentiments: { text: string; sentiment: string; score: number }[] = [];
+	const formatter = new Intl.DateTimeFormat('en-US', {
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		fractionalSecondDigits: 3
+	});
 
-	async function checkSentiment() {
-		inProgress = true;
+	// format date so it shows hour, minute, second and millisecond like this: HH:mm:ss.SSS
+	$: formattedDate = formatter.format(currentDate);
 
-		const { data } = await supabase.functions.invoke('sentiment', {
-			body: { input: currentMessage }
-		});
+	onMount(() => {
+		interval = setInterval(() => {
+			currentDate = new Date();
+		}, 11);
+	});
 
-		inProgress = false;
-		sentiments = [
-			...sentiments,
-			{ text: currentMessage, sentiment: data[0].label, score: data[0].score }
-		];
-		currentMessage = '';
-	}
+	onDestroy(() => {
+		clearInterval(interval);
+	});
 </script>
 
-<div class="flex h-screen">
-	<div class="w-1/3 flex items-center justify-center"></div>
-
-	<div class="w-1/3 flex items-center justify-center">
-		<Prompt
-			bind:value={currentMessage}
-			bind:inProgress
-			on:submit={checkSentiment}
-			placeholder="Enter a message to check sentiment"
-		/>
-	</div>
-
-	<div class="w-1/3 flex items-center justify-center">
-		<div class="table-container">
-			{#if sentiments.length}
-				<table class="table w-3/4 mx-auto">
-					<thead>
-						<th>Message</th>
-						<th>Sentiment</th>
-						<th>Score</th>
-					</thead>
-
-					<tbody>
-						{#each sentiments as { text, sentiment, score }}
-							<tr>
-								<td>{text}</td>
-								<td>
-									{#if sentiment === 'POSITIVE'}
-										<span class="badge variant-filled-success">POSITIVE</span>
-									{:else if sentiment === 'NEGATIVE'}
-										<span class="badge variant-filled-error">NEGATIVE</span>
-									{:else}
-										<span class="badge variant-filled-secondary">NEUTRAL</span>
-									{/if}
-								</td>
-								<td>
-									<pre>{Math.round(score * 10000) / 10000}</pre>
-								</td>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			{/if}
-		</div>
-	</div>
+<div class="flex justify-center items-center h-screen">
+	<pre>{formattedDate}</pre>
 </div>
