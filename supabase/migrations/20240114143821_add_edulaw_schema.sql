becb9ec2af2a994aee4de9d717c9119e78807009
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-14 17:34:18 +0100

    add temp migration

---
+++ supabase/migrations/20240114143821_add_edulaw_schema.sql
@@ -0,0 +1,26 @@
+create schema if not exists edulaw;
+
+create table edulaw.articles (
+  number text not null primary key,
+  content text not null,
+  embedding vector(768)
+);
+
+create table edulaw.acts (
+  number text not null,
+  article_number text not null,
+  content text not null,
+  embedding vector(768),
+  primary key (number, article_number)
+);
+
+create table edulaw.sections (
+  number text not null,
+  article_number text not null,
+  act_number text,
+  content text not null,
+  embedding vector(768),
+  primary key (number, act_number, article_number)
+);
+
+
