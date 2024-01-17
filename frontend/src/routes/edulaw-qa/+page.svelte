51fbcade1bf5bc5ae86d3468cf2090ca6b5540ae
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-17 02:18:25 +0100

    measure time during execution

--- frontend/src/routes/edulaw-qa/+page.svelte
+++ frontend/src/routes/edulaw-qa/+page.svelte
@@ -6,24 +6,30 @@
 	export let currentMessage: string;
 	export let inProgress: boolean;
 	let response: string = '';
+	let timeElapsedMs: number | undefined = undefined;
 
 	async function runChain() {
 		inProgress = true;
 		response = '...';
+		timeElapsedMs = 0;
 
+    const startTime = performance.now(); // Start the timer
 		const chain = new RemoteRunnable({ url: PUBLIC_EDULAW_URL });
 		const stream = await chain.stream({ question: currentMessage });
 
+		timeElapsedMs = performance.now() - startTime;
 		response = '';
 
 		for await (const chunk of stream) {
 			if (chunk) {
 				if (typeof chunk === 'string') {
 					response += chunk;
+					timeElapsedMs = performance.now() - startTime;
 				}
 			}
 		}
 
+		timeElapsedMs = performance.now() - startTime;
 		inProgress = false;
 		currentMessage = '';
 	}
@@ -31,8 +37,7 @@
 
 <div class="flex h-screen">
 	<div class="w-1/2 flex items-center justify-center">
-		<div class="mb-8 text-left p-4 h-24">
-		</div>
+		<div class="mb-8 text-left p-4 h-24"></div>
 
 		<Prompt
 			bind:value={currentMessage}
@@ -46,9 +51,10 @@
 
 	<div class="w-1/2 flex items-center justify-center">
 		<div class="w-3/4 mx-auto h-24 p-4 text-left">
+			{#if timeElapsedMs}
+				<p class="text-gray-200 text-xs">{timeElapsedMs}ms</p>
+			{/if}
 			{response}
 		</div>
 	</div>
 </div>
-
-
