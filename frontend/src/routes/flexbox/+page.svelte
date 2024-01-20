49c76b44ff0b6ecbbb8eabc211eac4a1aa72473b
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-20 17:35:39 +0100

    add flexbox page

---
+++ frontend/src/routes/flexbox/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col items-center h-screen">
+	<div class="flex-grow w-full">a</div>
+	<div class="flex-grow w-full order-1 bg-white">b</div>
+	<div class="flex-grow w-full">c</div>
+</div>
