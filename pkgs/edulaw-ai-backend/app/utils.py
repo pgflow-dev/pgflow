976e247733d43368a40a375fc562a4b94aade6b3
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-21 18:17:57 +0100

    edulaw-ai-backend: refactor init helpers

--- pkgs/edulaw-ai-backend/app/utils.py
+++ pkgs/edulaw-ai-backend/app/utils.py
@@ -1,12 +1,14 @@
 import os
 
 from app.prefixed_embeddings import PrefixedEmbeddings
+from dotenv import load_dotenv
 from langchain_community.vectorstores.supabase import SupabaseVectorStore
 from supabase.client import Client, create_client
 
 
-def init_supabase_client(supabase_url=os.environ['SUPABASE_URL'], supabase_key=os.environ['SUPABASE_KEY']) -> Client:
-    return create_client(supabase_url, supabase_key)
+def init_supabase_client() -> Client:
+    load_dotenv()
+    return create_client(os.environ['SUPABASE_URL'], os.environ['SUPABASE_KEY'])
 
 def init_supabase_vectorstore(
         supabase: Client = init_supabase_client(),
