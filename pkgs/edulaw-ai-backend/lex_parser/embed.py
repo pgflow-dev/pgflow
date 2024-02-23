076bce12cf566c373cd69e182f53289a219e9dd3
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-23 11:05:16 +0100

    embed without duplicates (do not keep separator)

--- pkgs/edulaw-ai-backend/lex_parser/embed.py
+++ pkgs/edulaw-ai-backend/lex_parser/embed.py
@@ -1,18 +1,20 @@
 import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from langchain_community.vectorstores.supabase import SupabaseVectorStore
-from supabase.client import Client, create_client
 from app.prefixed_embeddings import PrefixedEmbeddings
-from lex_parser.lex_parser_loader import LexParserLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores.supabase import SupabaseVectorStore
+from lex_parser.lex_parser_loader import LexParserLoader
+from supabase.client import Client, create_client
 
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=200,
     chunk_overlap=30,
     separators=[';'],
-    keep_separator=True
+    keep_separator=False
 )
 supabase: Client = create_client(os.environ['SUPABASE_URL'], os.environ['SUPABASE_KEY'])
 loader = LexParserLoader(path='data/educational-law-2024.txt')
