e03bf502ea9ca7a32d554ae0df485d483012be2a
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-23 13:54:21 +0100

    relation getters

--- pkgs/edulaw-ai-backend/lex_parser/db.py
+++ pkgs/edulaw-ai-backend/lex_parser/db.py
@@ -1,15 +1,55 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, TypeVar
 
 from lex_parser.models import *
 from lex_parser.parser import Parser
 
+T = TypeVar('T', bound='BaseDecorator')
 
 class BaseDecorator:
     def __getattr__(self, name):
         return getattr(self._model, name)
 
+    @property
+    def chapter(self):
+        try:
+            return self._db.chapters_by(chapter_no=self.chapter_no)[0]
+        except IndexError:
+            return None
+
+    @property
+    def articles(self):
+        return self._db.articles_by(**self.id)
+
+    @property
+    def points(self):
+        return self._db.points_by(**self.id)
+
+    @property
+    def paragraphs(self):
+        return self._db.paragraphs_by(**self.id)
+
+    @property
+    def paragraph(self):
+        return self.db.find_paragraph(self.paragraph_no)
+
+    @property
+    def article(self):
+        return self.db.find_article(self.article_no)
+
+    @property
+    def point(self):
+        return self.db.find_point(self.point_no)
+
+def matches_filter(model: BaseDecorator, filter: dict[str, str]) -> bool:
+    for key, value in filter.items():
+        if getattr(model, key) != value:
+            return False
+    return True
+
+def filter_by(models: List[T], filter: dict[str, str]) -> List[T]:
+    return [model for model in models if matches_filter(model, filter)]
 
 class ChapterDecorator(BaseDecorator):
     def __init__(self, model: Chapter, db: LexDb):
@@ -21,40 +61,20 @@ class ArticleDecorator(BaseDecorator):
         self._model = model
         self._db = db
 
-    @property
-    def chapter(self):
-        return self.db.find_chapter(self.chapter_no)
-
 class ParagraphDecorator(BaseDecorator):
     def __init__(self, model: Paragraph, db: LexDb):
         self._model = model
         self._db = db
-
-    @property
-    def article(self):
-        return self.db.find_article(self.article_no)
 
 class PointDecorator(BaseDecorator):
     def __init__(self, model: Point, db: LexDb):
         self._model = model
         self._db = db
 
-    @property
-    def paragraph(self):
-        return self.db.find_paragraph(self.paragraph_no)
-
-    @property
-    def article(self):
-        return self.db.find_article(self.article_no)
-
 class SubpointDecorator(BaseDecorator):
     def __init__(self, model: Subpoint, db: LexDb):
         self._model = model
         self._db = db
-
-    @property
-    def point(self):
-        return self.db.find_point(self.point_no)
 
 class LexDb:
     def __init__(
@@ -92,6 +112,21 @@ class LexDb:
     def subpoints(self) -> List[SubpointDecorator]:
         return [SubpointDecorator(subpoint, db=self) for subpoint in self._subpoints]
 
+    def chapters_by(self, **filter) -> List[ChapterDecorator]:
+        return filter_by(self.chapters, **filter)
+
+    def articles_by(self, **filter) -> List[ArticleDecorator]:
+        return filter_by(self.articles, **filter)
+
+    def paragraphs_by(self, **filter) -> List[ParagraphDecorator]:
+        return filter_by(self.paragraphs, **filter)
+
+    def points_by(self, **filter) -> List[PointDecorator]:
+        return filter_by(self.points, **filter)
+
+    def subpoints_by(self, **filter) -> List[SubpointDecorator]:
+        return filter_by(self.subpoints, **filter)
+
     @classmethod
     def from_parser(cls, parser: Parser):
         return cls(
@@ -109,4 +144,7 @@ if __name__ == '__main__':
     with open('data/educational-law-2024.txt', 'r') as file:
         parser.parse(file)
 
-    db = LexDb.from_parser(parser)
+    # db = LexDb.from_parser(parser)
+    # paragraph = db.find_paragraphs(chapter_no='10', article_no='')
+    #
+    # pprint(paragraph)
