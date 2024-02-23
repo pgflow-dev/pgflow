c59f59b57d49575b08f99a8de576fcbb0c4f4e4c
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-23 18:11:00 +0100

    sdxl spike in svelte (wip)

---
+++ pkgs/frontend/src/routes/spikes/sdxl/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Prompt from '$components/Prompt.svelte';
+
+	let storyPrompt = '';
+	let inProgress = false;
+	let prompts: Array<string> = [];
+
+	function onSubmit() {
+		prompts = [...prompts, storyPrompt];
+		storyPrompt = '';
+	}
+</script>
+
+<div class="flex flex-col container justify-center items-center">
+	<div class="m-8 container">
+		<Prompt
+			bind:value={storyPrompt}
+			placeholder="Write a story in one sentence here"
+			label="generate"
+			bind:inProgress
+			on:submit={onSubmit}
+		/>
+	</div>
+	<div>
+		<h3>Prompts</h3>
+		{#each prompts as prompt}
+			<pre>{prompt}</pre>
+		{/each}
+	</div>
+	<div></div>
+</div>
