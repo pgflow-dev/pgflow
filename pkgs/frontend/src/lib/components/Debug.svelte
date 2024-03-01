86314a12d1540260dbd1246632d00c1e425c559b
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-01 09:50:51 +0100

    add Debug component

---
+++ pkgs/frontend/src/lib/components/Debug.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let value: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+	export let label: string | null = null;
+</script>
+
+<div class="border border-white">
+	{#if label}
+		<span class="font-bold h4">{label}</span>
+	{/if}
+
+	<pre class="whitespace-pre p-4 overflow-x-auto">
+		{JSON.stringify(value, null, 2).trimStart()}
+	</pre>
+</div>
