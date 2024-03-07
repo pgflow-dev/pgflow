498debd967df8b8cfd0e42c87b400a86e692e898
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-07 16:41:58 +0100

    just redirect without throwing any errors

--- pkgs/frontend/src/routes/spikes/+layout.ts
+++ pkgs/frontend/src/routes/spikes/+layout.ts
@@ -1,12 +1,12 @@
 import type { LayoutLoadEvent, LayoutLoad } from './$types';
-import { fail } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { Session } from '@supabase/supabase-js';
 
 export const load: LayoutLoad = async (event: LayoutLoadEvent) => {
 	const parent = await event.parent();
 
 	if (!parent.session) {
-		throw fail(403, { message: 'Not logged in' });
+		redirect(302, '/auth/sign-in');
 	}
 
 	const session: Session = parent.session;
