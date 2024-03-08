906e4677fcdc01d8ab003a65ed9eb347d640a058
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-08 10:56:05 +0100

    small tweaks to the sign in page

--- pkgs/frontend/src/routes/auth/sign-in/+page.svelte
+++ pkgs/frontend/src/routes/auth/sign-in/+page.svelte
@@ -3,7 +3,7 @@
 	let { supabase } = data;
 	$: ({ supabase } = data);
 
-	let email = 'john.doe@example.com';
+	let email = '';
 	let inProgress = false;
 	let linkSent = false;
 
@@ -32,21 +32,28 @@
 	}
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen">
+<div class="flex flex-col items-center justify-center h-screen space-y-4">
 	{#if linkSent}
 		<h3 class="h3">Magic link sent!</h3>
 		<p>Check your email client and click the link to sign in</p>
 	{:else}
 		<h3 class="h3">Sign in with magic link</h3>
+		<p>Enter your email and we'll send you a magic link to sign in.</p>
 		<label class="label flex flex-col items-center">
-			<input class="input" type="text" placeholder="email" bind:value={email} />
+			<input
+				class="input"
+				type="text"
+				placeholder="email"
+				disabled={inProgress}
+				bind:value={email}
+			/>
 		</label>
 
 		<button
 			on:click={signInWithEmail}
 			disabled={!isValidEmail(email) || inProgress}
 			value={email}
-			class="btn btn-xl variant-filled"
+			class="btn variant-filled"
 		>
 			Send magic link
 		</button>
