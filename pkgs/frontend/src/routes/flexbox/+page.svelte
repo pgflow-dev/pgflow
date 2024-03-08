f813b6932fa5672dc9ee7b4934056389b74ef149
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-08 09:25:38 +0100

    remove dead or unused pages/spikes

--- pkgs/frontend/src/routes/flexbox/+page.svelte
+++
@@ -1,5 +0,0 @@
-<div class="flex flex-col items-center h-full">
-	<div class="flex-grow w-full">a</div>
-	<div class="flex-grow w-full order-1 bg-white">b</div>
-	<div class="flex-grow w-full">c</div>
-</div>
