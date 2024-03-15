ef54cf9ec07b1b6904d3cda003b5ed7e271f1701
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-15 12:16:33 +0100

    key messages

--- pkgs/frontend/src/lib/components/ChatMessageList.svelte
+++ pkgs/frontend/src/lib/components/ChatMessageList.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="grid grid-cols-[auto_1fr] gap-2 w-full md:w-3/4">
-	{#each $messagesStore as message}
+	{#each $messagesStore as message (message)}
 		<div class="font-bold">{message.role}:</div>
 		<div class="">{message.content}</div>
 	{/each}
