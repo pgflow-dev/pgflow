42a3910daaf9bea41c2600178210257bb15fb7dc
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-18 11:40:41 +0100

    wip

--- pkgs/frontend/src/routes/spikes/chat-layout-diy/+page.svelte
+++ pkgs/frontend/src/routes/spikes/chat-layout-diy/+page.svelte
@@ -3,29 +3,54 @@
 	import Prompt from '$components/Prompt.svelte';
 
 	let currentMessage = '';
+
+	const LOREM = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor
+	sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+	Repellat, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor
+	sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor sit amet consectetur
+	adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptate.
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+	adipisicing elit. Repellat, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor
+	sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+	Repellat, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor
+	sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor sit amet consectetur
+	adipisicing elit.`;
 </script>
 
-<div class="flex flex-col h-full justify-center p-6">
-	<div class="flex-grow w-[75vw] mx-auto">
-		<div class="w-full p-4 bg-surface-500 rounded-xl m-2">
-			<div class="h4 flex items-center">
-				<Avatar width="w-8" name="You" class="mr-4" />
+<div class="h-full grid grid-rows-[auto_1fr] gap-1">
+	<div class="h-full bg-surface-500/30 p-4 w-[60vw] mx-auto overflow-x-hidden overflow-y-scroll">
+		<div class="w-full p-4 bg-surface-500 rounded-xl m-2 text-justify">
+			<div class="font-bold flex items-center mb-2">
+				<Avatar width="w-4" name="You" class="mr-4" />
 				You
 			</div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor
-			sit amet consectetur adipisicing elit.
+			{LOREM}
 		</div>
-		<div class="w-full p-4 bg-surface-700 rounded-xl m-2">
-			<div class="h4 flex items-center">
-				<Avatar width="w-8" name="Assistant" class="mr-4" />
+		<div class="w-full p-4 bg-surface-700 rounded-xl m-2 text-justify">
+			<div class="font-bold flex items-center">
+				<Avatar width="w-4" name="Assistant" class="mr-4" />
 				Assistant
 			</div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, voluptate. Lorem ipsum dolor
-			sit amet consectetur adipisicing elit.
+			{LOREM}
+		</div>
+		<div class="w-full p-4 bg-surface-500 rounded-xl m-2 text-justify">
+			<div class="font-bold flex items-center mb-2">
+				<Avatar width="w-4" name="You" class="mr-4" />
+				You
+			</div>
+			{LOREM}
+		</div>
+		<div class="w-full p-4 bg-surface-700 rounded-xl m-2 text-justify">
+			<div class="font-bold flex items-center">
+				<Avatar width="w-4" name="Assistant" class="mr-4" />
+				Assistant
+			</div>
+			{LOREM}
 		</div>
 	</div>
 
-	<div class="mx-auto w-[75vw] border border-red-400">
+	<div class="bg-surface-500/30 p-4 w-[60vw] mx-auto">
 		<Prompt label="send" placeholder="ask a question" value={currentMessage} />
 	</div>
 </div>
