e08f56c46d1762f4b8734e9d5ce4159ae032e032
Wojciech Majewski <wojciech.majewski@pm.me>
2024-04-05 17:26:20 +0200

    update seeds with friends' emails

--- pkgs/supabase/seed.sql
+++ pkgs/supabase/seed.sql
@@ -1,3 +1,6 @@
 insert into acl.superadmin_emails (email) values
-  ('admin@example.com')
+  ('admin@example.com'),
+  ('wojciech.majewski@pm.me'),
+  ('majewskibartosz@pm.me'),
+  ('amadeusz.filipek@protonmail.com')
   on conflict do nothing;
