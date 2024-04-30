8857d43583852f8a01105155c553cbaf8ebf318f
Wojciech Majewski <wojciech.majewski@pm.me>
2024-04-30 11:54:05 +0200

    add InlineStatusIndicator

---
+++ pkgs/frontend/src/lib/components/InlineStatusIndicator.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
+	export let label = 'Understanding question...';
+	export let loading = true;
+</script>
+
+<div class="flex">
+	<div class="w-5">
+		{#if loading}
+			<ProgressRadial width="w-5" stroke={100} />
+		{:else}
+			✅
+		{/if}
+	</div>
+	<span class="pl-2 font-bold">{label}</span>
+</div>
---
+++ pkgs/frontend/src/stories/InlineStatusIndicator.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Hst as HstType } from '@histoire/plugin-svelte';
+	import InlineStatusIndicator from '$components/InlineStatusIndicator.svelte';
+
+	export let Hst: HstType;
+
+	let loading = true;
+	let label = 'Understanding question...';
+</script>
+
+<Hst.Story title="InlineStatusIndicator" layout={{ type: 'single', iframe: true }}>
+	<Hst.Variant title="Default">
+		<InlineStatusIndicator {loading} {label} />
+
+		<svelte:fragment slot="controls">
+			<Hst.Checkbox title="In Progress" bind:value={loading} />
+			<Hst.Text title="Label" bind:value={label} />
+		</svelte:fragment>
+	</Hst.Variant>
+</Hst.Story>
