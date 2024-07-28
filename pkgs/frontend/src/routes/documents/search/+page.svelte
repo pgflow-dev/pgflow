df745a382dc4ee81fba8c25e955a5089411c6efa
Wojtek Majewski <wojciech.majewski@pm.me>
2024-07-28 15:36:49 +0200

    working full text search

---
+++ pkgs/frontend/src/routes/documents/search/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import Prompt from '$components/Prompt.svelte';
+	import { writable } from 'svelte/store';
+
+	export let data;
+	let { session, supabase } = data;
+	$: ({ session, supabase } = data);
+
+	type Doc = { id: number; content: string };
+
+	const input = writable('');
+	const loading = writable(false);
+	const documents = writable<Doc[]>([]);
+
+	const handleSubmit = async () => {
+		const searchQuery = $input;
+		$input = '';
+
+		loading.set(true);
+
+		const docs = await supabase
+			.from('documents')
+			.select('*')
+			.textSearch('content', `'${searchQuery}'`, { type: 'websearch' })
+			.limit(5)
+			.returns<Doc[]>();
+
+		if (docs.data) {
+			documents.set(docs.data);
+		}
+
+		loading.set(false);
+	};
+</script>
+
+<div class="flex flex-col gap-4">
+	<Prompt
+		bind:value={$input}
+		on:submit={handleSubmit}
+		loading={$loading}
+		placeholder="Search docs..."
+	/>
+</div>
+
+<ul>
+	{#each $documents as doc (doc)}
+		<li class="pb-4">
+			{doc.content}
+		</li>
+	{/each}
+</ul>
