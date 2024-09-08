08c551e309d9f4f98c297bbbde6c2e111df12f61
Wojtek Majewski <wojciech.majewski@pm.me>
2024-09-08 03:37:33 +0200

    feat: update commit message generation script to use gpt-4o-mini model
    - Changed the model used for generating commit messages to gpt-4o-mini.
    - Improved the logic for handling the commit message generation based on the diff length.

--- .lefthook/prepare-commit-msg/generate-commit-msg.sh
+++ .lefthook/prepare-commit-msg/generate-commit-msg.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
+openai_model=gpt-4o-mini
+
 COMMIT_MSG_FILE="$1"
 COMMIT_SOURCE="$2"
-PROMPT_TEXT="
-Based on the changes above, generate commit message that adheres to Conventional Commit specification.
-Do not write anything about generating commits or any meta informations. The only thing required from you is the content of the commit itself, starting with commit type, as in Conventional Commit.
-
-DO NOT include backticks
-DO NOT include markdown formatting
-DO NOT write about generating commits
-
-DO start with the commit type based on conventional commit
-"
 
 if [ -z "$COMMIT_SOURCE" ]; then
     diff=$(git diff --cached)
     diff_length=${#diff}
 
-    if [ $diff_length -lt 5000 ]; then
-        echo "$diff" |
-            sgpt --model gpt-4o "$PROMPT_TEXT" \
-            > "$COMMIT_MSG_FILE"
+    function generate_with() {
+        local model=${1:$weak_model}
+     
+        echo $diff | sgpt --model $model --role commit-msg | sed 's/^\s*```//;s/```\s*$//' | awk 'NF {p=1} p; {if (NF) {p=1}}'
+    }
+
+    if [ $diff_length -lt 200000 ]; then # ~50k input tokens, 0.0075$
+        generate_with $openai_model > "$COMMIT_MSG_FILE"
     else
         echo "<<< DIFF TOO LONG - message generation skipped >>>" > "$COMMIT_MSG_FILE"
     fi
