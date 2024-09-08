6095b4db3fa7f74e78ea9c1f02f263a0c9f90b50
Wojtek Majewski <wojciech.majewski@pm.me>
2024-09-08 02:47:26 +0200

    chore: update commit message generation script

--- .lefthook/prepare-commit-msg/generate-commit-msg.sh
+++ .lefthook/prepare-commit-msg/generate-commit-msg.sh
@@ -6,18 +6,20 @@ PROMPT_TEXT="
 Based on the changes above, generate commit message that adheres to Conventional Commit specification.
 Do not write anything about generating commits or any meta informations. The only thing required from you is the content of the commit itself, starting with commit type, as in Conventional Commit.
 
-If the change is big, write a very concise summary of changes as a bullet point list.
+DO NOT include backticks
+DO NOT include markdown formatting
+DO NOT write about generating commits
 
-ABSOLUTELY DO NOT INCLUDE BACKTICKS OR MARKDOWN FORMATTING: you are only allowed to start your reply with one of the commit types."
+DO start with the commit type based on conventional commit
+"
 
-# Only pre-fill if it's a regular commit (not an amend, merge, etc.)
 if [ -z "$COMMIT_SOURCE" ]; then
     diff=$(git diff --cached)
     diff_length=${#diff}
 
     if [ $diff_length -lt 5000 ]; then
         echo "$diff" |
-            sgpt "$PROMPT_TEXT" \
+            sgpt --model gpt-4o "$PROMPT_TEXT" \
             > "$COMMIT_MSG_FILE"
     else
         echo "<<< DIFF TOO LONG - message generation skipped >>>" > "$COMMIT_MSG_FILE"
