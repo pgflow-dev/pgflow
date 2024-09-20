a6b2426c511d3cbce157e474993aa7dcf5e6e0f8
Wojtek Majewski <wojciech.majewski@pm.me>
2024-09-20 14:10:37 +0200

    feat(feed): implement semantic search with results display

--- pkgs/frontend/src/routes/feed/notes/+page.svelte
+++ pkgs/frontend/src/routes/feed/notes/+page.svelte
@@ -2,8 +2,7 @@
 	import Prompt from '$components/Prompt.svelte';
 	import type { FeedNoteRow } from '$lib/db';
 	import { writable } from 'svelte/store';
-	// this is doc semantic search with prompt/input on top, list on full screen, filters in right sidebar
-	// filters is 1/8
+	import { fly, fade } from 'svelte/transition';
 
 	export let data;
 	let { supabase } = data;
@@ -11,7 +10,7 @@
 
 	const searchTerm = writable('');
 	const notes = writable<FeedNoteRow[]>([]);
-
+	let showResults = false;
 	async function fetchRelevantNotes() {
 		const response = await supabase.schema('feed').rpc('easy_match_notes', {
 			query: $searchTerm,
@@ -25,12 +24,13 @@
 		const data = response?.data || [];
 
 		notes.set(data);
+		showResults = true;
 	}
 </script>
 
-<div class="container mx-auto px-4">
-	<div class="grid grid-cols-12 gap-4">
-		<div class="col-span-6 col-start-4 flex mb-4 pt-10 pb-4">
+<div class="container mx-auto px-4 h-screen flex flex-col">
+	<div class="flex-grow flex items-center justify-center" class:hidden={showResults}>
+		<div class="w-1/2">
 			<Prompt
 				value={$searchTerm}
 				label="search"
@@ -38,16 +38,33 @@
 				on:submit={fetchRelevantNotes}
 			/>
 		</div>
-
-		<div class="col-span-10 col-start-2">
-			{#each $notes as note (note)}
-				<div class="card mb-4">
-					<header class="card-header">
-						<a class="flex-grow" href="/feed/notes/{note.id}">{note.id}</a>
-					</header>
-					<section class="p-4">{note.content}</section>
-				</div>
-			{/each}
-		</div>
 	</div>
+
+	{#if showResults}
+		<div class="grid grid-cols-12 gap-4">
+			<div
+				class="col-span-6 col-start-4 flex mb-4 pt-10 pb-4"
+				in:fly={{ y: 500, duration: 100 }}
+				out:fade
+			>
+				<Prompt
+					value={$searchTerm}
+					label="search"
+					placeholder="search for notes"
+					on:submit={fetchRelevantNotes}
+				/>
+			</div>
+
+			<div class="col-span-10 col-start-2" in:fade={{ duration: 100, delay: 0 }}>
+				{#each $notes as note (note)}
+					<div class="card mb-4">
+						<header class="card-header">
+							<a class="flex-grow" href="/feed/notes/{note.id}">{note.id}</a>
+						</header>
+						<section class="p-4">{note.content}</section>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/if}
 </div>
