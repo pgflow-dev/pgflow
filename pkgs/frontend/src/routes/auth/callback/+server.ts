bd3c50c460b9f7f27bfe21ef0599d45a0e80a83e
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-03 13:22:38 +0200

    feat(auth): add detailed logging for auth callback process

--- pkgs/frontend/src/routes/auth/callback/+server.ts
+++ pkgs/frontend/src/routes/auth/callback/+server.ts
@@ -9,12 +9,28 @@ export const GET = async (event: RequestEvent) => {
 	const code = url.searchParams.get('code') as string;
 	const next = url.searchParams.get('next') ?? '/';
 
+	console.log('/auth/callback', {
+		url,
+		params: event.params,
+		searchParams: url.searchParams,
+		code,
+		next
+	});
+
 	if (code) {
-		const { error } = await supabase.auth.exchangeCodeForSession(code);
+		console.log('/auth/callback - CODE IS PRESENT', code);
+		const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+		console.log('/auth/callback - AFTER exchangeCodeForSession', { data, error });
+
+		const sess = await supabase.auth.getSession();
+		console.log('/auth/callback - getSession()', sess);
+
 		if (!error) {
 			throw redirect(303, `/${next.slice(1)}`);
 		}
 	}
+
+	console.log('/auth/callback - NO CODE!!!!!');
 
 	// return the user to an error page with instructions
 	throw redirect(303, '/auth/auth-code-error');
