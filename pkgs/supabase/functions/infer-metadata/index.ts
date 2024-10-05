f1d41ee4fba9a9e13510d82cb85259fe6ba33654
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-05 09:37:38 +0200

    feat(metadata): enhance UI schema with additional types and improve documentation

--- pkgs/supabase/functions/infer-metadata/index.ts
+++ pkgs/supabase/functions/infer-metadata/index.ts
@@ -3,61 +3,114 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import "npm:openai";
 import { z } from "npm:zod";
 import OpenAI from "npm:openai@^4.52.5";
+import { zodResponseFormat } from "npm:openai/helpers/zod";
 
 const client = new OpenAI({ apiKey: Deno.env.get("OPENAI_API_KEY") });
 
+// # 'ui' key
+// An object with required 'id' key. any additional key is a param for the UI component that will render the metadata.
+// I will list all possible ui values below, listing UI component params in brackets, like this: ui_key(param1, param2)
+// Type of UI that should be used to render this item. Must be one of the following:
+// - 'event(datetime, title, place, description)' - for any calendar-like events with specific date and optional time
+// - 'video(url, title)' - for any youtube and other videos
+// - 'snippet(source, language_code)' - for any code or monospace related content
+// - 'card(header, emoji, text)' - for things like stars, likes, persons, items, bookmarks, everything that should stand next to icon/emoji and stand out
+// - 'action(text, emoji)' - for anything actionable that can be "COMPLETED", like todo items, movie "watch later" items etc.
+// - 'text(text)' - just text, for everything else
+const UI = {
+  Event: z.object({
+    type: z.literal("event"),
+    datetime: z.string(),
+    title: z.string(),
+    place: z.string(),
+    description: z.string(),
+  }),
+  Video: z.object({
+    type: z.literal("video"),
+    url: z.string(),
+    title: z.string(),
+  }),
+  Snippet: z.object({
+    type: z.literal("snippet"),
+    source: z.string(),
+    language_code: z.string(),
+  }),
+  Card: z.object({
+    type: z.literal("card"),
+    header: z.string(),
+    emoji: z.string(),
+    text: z.string(),
+  }),
+  Action: z.object({
+    type: z.literal("action"),
+    completed: z.boolean(),
+    text: z.string(),
+  }),
+  Bookmark: z.object({
+    type: z.literal("bookmark"),
+    url: z.string(),
+    title: z.string(),
+  }),
+  Text: z.object({
+    type: z.literal("text"),
+    text: z.string(),
+  }),
+};
 const NoteMetadataSchema = z.object({
   type: z.enum(["github_star", "note", "todo", "bookmark"]),
   value: z.string(),
   keywords: z.array(z.string()),
+  ui: z.union([
+    UI.Event,
+    UI.Video,
+    UI.Snippet,
+    UI.Card,
+    UI.Action,
+    UI.Bookmark,
+    UI.Text,
+  ]),
 });
 type StructuredOutput = z.infer<typeof NoteMetadataSchema>;
 
 const SYSTEM_PROMPT = `
-You are skilled metadata extractor. You must extract few pieces of metadata
-based on description of each piece and then output those as JSON object
-with keys as metadata names and values as metadata values.
+You are skilled UX designer. You will get random piece of saved text, that
+comes mostly from Share Target API, but can albo be pasted.
 
-# 'type' key
-Type can be one of following:
-- github_star
-- note
-- todo
-- bookmark
+Your job is to guess, what kind of content is this and what kind of
+UI component would be best suited to show this particular content.
 
-# 'value' key
-Each type have its "value" and for each type it must be something type specific:
+Output only valid JSON according to provided schema, with following field definitions:
 
-- github_star has the repo id in value: "rails/rails", "reactjs/react-tabs" etc.
-- note has the note body text in value
-- todo has the todo body text in value
-- bookmark has the url in value
-
-# 'keywords' key
-An array of strings - each string is a keyword. Use only good matching keywords.
-For example, for type github_star with value of rails/rails, you would extract
-["rails", "ruby", "http", "database"]
+# 'keywords'
+You extract keywords relevant to the content itself.
+Keywords is just an array of strings, each string is a keyword.
+Use only good matching keywords.
 
 # 'ui' key
-An object with required 'id' key. any additional key is a param for the UI component that will render the metadata.
-I will list all possible ui values below, listing UI component params in brackets, like this: ui_key(param1, param2)
+An object with required 'type' key. any additional key is a param for the UI component that will render the metadata.
+'type' key determines what kind of UI is best to render this particular content type.
+All possible 'type' values below, listing UI component params in brackets, like this: type(param1, param2)
 Type of UI that should be used to render this item. Must be one of the following:
 - 'event(datetime, title, place, description)' - for any calendar-like events with specific date and optional time
 - 'video(url, title)' - for any youtube and other videos
 - 'snippet(source, language_code)' - for any code or monospace related content
 - 'card(header, emoji, text)' - for things like stars, likes, persons, items, bookmarks, everything that should stand next to icon/emoji and stand out
 - 'action(text, emoji)' - for anything actionable that can be "COMPLETED", like todo items, movie "watch later" items etc.
+- 'bookmark(title, url)' - for any link that was not recognized as more specific UI type. Must have URL, Title is optional but encouraged.
 - 'text(text)' - just text, for everything else
 
 Example extracted metadata:
 
 {
-  "type": "github_star",
-  "value": "rails/rails",
-  "keywords": ["rails", "ruby", "http", "database"],
+  "type": "bookmark",
+  "value": "https://github.com/rails/rails",
+  "keywords": ["rails", "ruby", "http", "database", "github"],
+  "ui": {
+    "type": "bookmark",
+    "title": "Ruby On Rails - GitHub",
+    "url": "https://github.com/rails/rails"
+  }
 }
-
-Make sure to output valid JSON.
 `;
 
 Deno.serve(async (req: Request) => {
@@ -69,7 +122,7 @@ Deno.serve(async (req: Request) => {
     ],
     // model: "gpt-4o",
     model: "gpt-4o-mini-2024-07-18",
-    response_format: { type: "json_object" },
+    response_format: zodResponseFormat(NoteMetadataSchema, "note-metadata"),
   });
   const content = completion.choices[0]?.message?.content;
 
