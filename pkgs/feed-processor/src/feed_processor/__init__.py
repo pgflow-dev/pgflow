9e2f3ed63f1219c9b80a6401621a6c71f5015d6b
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-15 10:09:36 +0200

    chore(feed-processor): simplify db init, remove dead code

--- pkgs/feed-processor/src/feed_processor/__init__.py
+++ pkgs/feed-processor/src/feed_processor/__init__.py
@@ -1,17 +1,26 @@
 from __future__ import annotations
 
+import os
+
+import asyncpg
 from feed_processor.actors.infer_type import infer_type
 from feed_processor.actors.infer_ui import infer_ui
 from feed_processor.supabase import create_service_role_client
-from feed_processor.utils import get_connection
 from pgqueuer.db import AsyncpgDriver
 from pgqueuer.models import Job
 from pgqueuer.qm import QueueManager
 from pgqueuer.queries import Queries
 
+print(f"LANGCHAIN_API_KEY={os.environ['LANGCHAIN_API_KEY']}")
+print(f"LANGCHAIN_ENDPOINT={os.environ['LANGCHAIN_ENDPOINT']}")
+print(f"LANGCHAIN_PROJECT={os.environ['LANGCHAIN_PROJECT']}")
+print(f"LANGCHAIN_TRACING_V2={os.environ['LANGCHAIN_TRACING_V2']}")
+print(f"DATABASE_URL={os.environ['DATABASE_URL']}")
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 async def main() -> QueueManager:
-    connection = await get_connection()
+    connection = await asyncpg.connect(DATABASE_URL)
     driver = AsyncpgDriver(connection)
     qm = QueueManager(driver)
     queries = Queries(driver)
