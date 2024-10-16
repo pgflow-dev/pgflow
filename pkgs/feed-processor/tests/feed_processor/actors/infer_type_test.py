6696e17caeceaede8fe1e56f95f0f07596c54c76
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-16 15:25:23 +0200

    test: migrate from unittest to pytest for infer_type tests, add more examples

--- pkgs/feed-processor/tests/feed_processor/actors/infer_type_test.py
+++ pkgs/feed-processor/tests/feed_processor/actors/infer_type_test.py
@@ -1,20 +1,33 @@
 import os
-import unittest
 
+import pytest
 from dotenv import load_dotenv
 from feed_processor.actors.infer_type.chain import create_chain
 
+bookmarks_input = [
+    ("[Hacker News](https://news.ycombinator.com)", "bookmark"),
+    ("[Example.com](https://example.com)", "bookmark"),
+    ("www.google.com", "bookmark"),
+    ("reddit.com/r/mildlyinteresting", "bookmark")
+]
 
-class TestInferType(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        load_dotenv()
-        cls.chain = create_chain(os.environ["OPENAI_API_KEY"])
+non_bookmark_input = [
+    "Then i watched the video at https://youtube.com?v=JxdFwIXdwYc",
+    "go to google.com in order to search for something",
+    "visit my website mywebsite.com in order to learn more about me",
+]
 
-    def test_snippet(self):
-        results = self.chain.invoke({"input": "[Hacker News](https://news.ycombinator.com)"})
+@pytest.fixture(scope="module")
+def chain():
+    load_dotenv()
+    return create_chain(os.environ["OPENAI_API_KEY"])
 
-        self.assertEqual(results.type, "bookmark")
+@pytest.mark.parametrize("input_text, expected_type", bookmarks_input)
+def test_bookmark(chain, input_text, expected_type):
+    results = chain.invoke({"input": input_text})
+    assert results.type == expected_type
 
-if __name__ == '__main__':
-    unittest.main()
+# @pytest.mark.parametrize("input_text", non_bookmark_input)
+# def test_non_bookmark(chain, input_text):
+#     results = chain.invoke({"input": input_text})
+#     assert results.type != "bookmark"
