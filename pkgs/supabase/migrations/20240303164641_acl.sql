dbe97bcea10827ecad3969ee9df314a91d931692
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-18 00:06:24 +0200

    feat(migrations): add trigger to enforce superadmin email validation in auth.users table

--- pkgs/supabase/migrations/20240303164641_acl.sql
+++ pkgs/supabase/migrations/20240303164641_acl.sql
@@ -5,7 +5,6 @@ create table if not exists acl.superadmin_emails
     email text primary key
 );
 
--- postgres function that checks if current signed in user's email is present in superadmin_emails table
 drop function if exists public.is_superadmin();
 create or replace function public.is_superadmin()
 returns boolean as $$
@@ -23,3 +22,20 @@ begin
   );
 end;
 $$ language plpgsql security definer;
+
+-- Create a trigger function to check if the email is in acl.superadmin_emails
+create or replace function public.check_superadmin_email()
+returns trigger as $$
+begin
+  if not exists (select 1 from acl.superadmin_emails where email = new.email) then
+    raise exception 'email % is not in the superadmin_emails table', new.email;
+  end if;
+  return new;
+end;
+$$ language plpgsql security definer;
+
+-- create a trigger on auth.users table
+create trigger enforce_superadmin_email
+before insert or update on auth.users
+for each row
+execute function public.check_superadmin_email();
