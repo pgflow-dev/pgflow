4603eb724e5a835b4e2e4ddbd16bc3ab62a2e95f
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-19 01:57:25 +0200

    feat: refactor TodoComponent to improve checkbox layout and styling

--- pkgs/frontend/src/lib/components/feed/TodoComponent.svelte
+++ pkgs/frontend/src/lib/components/feed/TodoComponent.svelte
@@ -4,12 +4,9 @@
 	export let todo: Todo;
 </script>
 
-<div
-	class="overflow-x-auto card break-inside-avoid p-2 variant-soft-secondary relative border border-gray-600"
+<label
+	class="overflow-x-auto card break-inside-avoid p-2 variant-soft-secondary relative border border-gray-600 flex items-center space-x-2"
 >
-	<!-- huge checkbox -->
-	<label>
-		<input type="checkbox" />
-		<span>{todo.title}</span>
-	</label>
-</div>
+	<input type="checkbox" class="h-4 w-4" />
+	<span>{todo.title}</span>
+</label>
