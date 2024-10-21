09d938685966a818c3b323a447cd5feb745b9643
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-22 00:52:03 +0200

    refactor: use llama3 8b finetuned for tool calling

--- pkgs/feed-processor/src/feed_processor/actors/extract_entity_by_type/chain.py
+++ pkgs/feed-processor/src/feed_processor/actors/extract_entity_by_type/chain.py
@@ -9,14 +9,24 @@ from .schema import RunnableInput, RunnableOutput
 
 
 def create_chain(context: JobContext) -> Runnable[RunnableInput, RunnableOutput]:
-    # model = ChatOpenAI(model='gpt-4o', api_key=api_key)
-    model = ChatGroq(model='gemma2-9b-it', api_key=context.groq_api_key, stop_sequences=[])
+    # model = ChatOpenAI(model='gpt-4o-mini', api_key=context.openai_api_key)
+
+    # llama3_70b_tool = "llama3-groq-70b-8192-tool-use-preview"
+    llama3_8b_tool = "llama3-groq-8b-8192-tool-use-preview"
+    # mixtral = "mixtral-8x7b-32768"
+    # gemma2 = "gemma2-9b-it"
+    model = ChatGroq(model=llama3_8b_tool, api_key=context.groq_api_key, stop_sequences=[])
 
     return RunnableSequence(
         prompt,
             model.with_structured_output(
             schema=RunnableOutput,
-            method="function_calling",
+
+            # openai
+            # method="json_schema",
             # strict=True
+
+            # groq
+            method="function_calling",
         )
     ).with_types(input_type=RunnableInput, output_type=RunnableOutput)
