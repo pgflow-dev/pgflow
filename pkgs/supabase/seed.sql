215f622c4f06e3702c619a3450c950bbbd195565
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-25 10:45:11 +0200

    feat(supabase): add new superadmin email to seed data

--- pkgs/supabase/seed.sql
+++ pkgs/supabase/seed.sql
@@ -1,5 +1,6 @@
 insert into acl.superadmin_emails (email) values
 ('admin@example.com'),
 ('wojciech.majewski@pm.me'),
-('jumski@gmail.com')
+('jumski@gmail.com'),
+('gajewsky@pm.me')
 on conflict do nothing;
