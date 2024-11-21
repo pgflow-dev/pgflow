bb9d83f2cb70f49da62cabb2dba867a2ec9911de
Wojtek Majewski <wojciech.majewski@pm.me>
2024-11-21 18:03:31 +0100

    feat(supabase): add type annotations to Supabase client functions
    - Added type annotations for `SupabaseClient` and `Database` to improve type safety.
    - Updated `createAuthenticatedClient` and `createServiceRoleClient` functions to utilize these types
    .

--- pkgs/supabase/functions/_shared/supabaseClient.ts
+++ pkgs/supabase/functions/_shared/supabaseClient.ts
@@ -1,15 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "../../types.d.ts";
 
 export function createAuthenticatedClient(req: Request) {
   const authHeader = req.headers.get("Authorization")!;
-  return createClient(
+  return createClient<Database>(
     Deno.env.get("API_URL") ?? "",
     Deno.env.get("ANON_KEY") ?? "",
     { global: { headers: { Authorization: authHeader } } },
   );
 }
 
-export function createServiceRoleClient() {
+export function createServiceRoleClient(): SupabaseClient {
   const API_URL = "http://host.docker.internal:54321";
 
   return createClient(API_URL, Deno.env.get("SERVICE_ROLE_KEY") ?? "");
