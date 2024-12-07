62159f72751508739c5ffb4c44bb0bdff3475eca
Wojtek Majewski <wojciech.majewski@pm.me>
2024-12-07 19:04:29 +0100

    refactor(pgflow): remove createConnectionPool function
    The createConnectionPool.ts file has been deleted as part of a refactor to streamline the connection
     handling process. This change removes the redundant code and simplifies the connection management l
    ogic.
    
    chore(pgflow): add deno.json and deno.lock files
    
    Added deno.json and deno.lock files to manage dependencies and ensure consistent builds across envir
    onments. These files will help in maintaining the integrity of the project dependencies.

---
+++ pkgs/supabase/functions/_pgflow/useConnectionPool.ts
@@ -0,0 +1,26 @@
+import { Pool } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
+
+export async function useConnectionPool() {
+  const pool = new Pool(
+    "postgresql://postgres:postgres@host.docker.internal:54322/postgres",
+    5,
+  );
+
+  async function withPostgres(callback: (connection: any) => Promise<any>) {
+    let connection;
+
+    try {
+      connection = await pool.connect();
+      console.log("Connected to connection", connection);
+
+      callback(connection);
+    } catch (error) {
+      console.error(error);
+      return;
+    } finally {
+      connection?.release();
+    }
+  }
+
+  return { pool, withPostgres };
+}
