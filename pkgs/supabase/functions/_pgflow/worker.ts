57c3099c91296c60bcae0504f7b4ca73e2a727ff
Wojtek Majewski <wojciech.majewski@pm.me>
2024-12-08 01:47:12 +0100

    feat(worker): enhance message processing with handler function
    - Added a `handler` function parameter to `startWorker` and `startWorkers` for custom message proces
    sing.
    - Changed `queryObject` to `queryArray` for fetching messages in `readMessages`.
    - Updated queue name from 'yolo' to 'pgflow' for consistency.
    - Improved logging for better traceability of worker operations.

--- pkgs/supabase/functions/_pgflow/worker.ts
+++ pkgs/supabase/functions/_pgflow/worker.ts
@@ -1,37 +1,49 @@
 import { useConnectionPool } from "./useConnectionPool.ts";
+import { Json } from "./Flow.ts";
 
 async function* readMessages(
   queueName: string,
   batchSize = 2,
   visibilityTimeout = 1,
 ) {
-  const { queryObject, withPostgres } = await useConnectionPool();
+  const { queryArray, withPostgres } = await useConnectionPool();
 
   while (true) {
-    const results = await queryObject(
-      `select now() as time`,
+    const results = await queryArray(
+      `select now() as time from generate_series(1, ${batchSize})`,
       // `SELECT pgmq.read('${queueName}', ${batchSize}, ${visibilityTimeout});`,
     );
-    yield results;
+    const { rows: messages } = results;
+    console.log(`WORKER: Messages`, messages);
+
+    for (const message in messages) {
+      yield message;
+    }
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 }
 
-export async function startWorker(slug: string) {
-  console.log(`WORKER ${slug}: Started`);
+export async function startWorker(
+  slug: string,
+  handler: (payload: Json) => void,
+) {
+  console.log(`${slug}: Started`);
   const { queryObject } = await useConnectionPool();
 
-  await queryObject(`SELECT pgmq.create('yolo');`);
+  await queryObject(`SELECT pgmq.create('pgflow');`);
 
-  for await (const results of readMessages("yolo")) {
-    console.log(`WORKER ${slug}: Results`, results);
+  for await (const results of readMessages("pgflow")) {
+    console.log(`${slug}:`, results);
   }
 }
 
-export async function startWorkers(count: number) {
+export async function startWorkers(
+  count: number,
+  handler: (payload: Json) => void,
+) {
   const workers = Array.from({ length: count }).map((_, index) => {
     const slug = `worker-${index + 1}`;
-    return startWorker(slug);
+    return startWorker(slug, handler);
   });
 
   return Promise.all(workers);
