18dfae72037dca700af32a939db0e2c6b0ee9551
Wojtek Majewski <wojciech.majewski@pm.me>
2024-12-17 02:15:14 +0100

    feat(pgflow): setup a simple pg connection with NOTIFY in pgflow-worker

---
+++ pkgs/supabase/functions/_pgflow/sql.ts
@@ -0,0 +1,7 @@
+import postgres from "https://deno.land/x/postgresjs/mod.js";
+
+const sql = postgres(
+  "postgresql://postgres:postgres@host.docker.internal:54322/postgres",
+);
+
+export default sql;
--- pkgs/supabase/functions/pgflow-worker/index.ts
+++ pkgs/supabase/functions/pgflow-worker/index.ts
@@ -1,8 +1,16 @@
 // import "@supabase/functions-js/edge-runtime.d.ts";
-import { startWorkers } from "../_pgflow/worker.ts";
+// import { startWorkers } from "../_pgflow/worker.ts";
+import sql from "../_pgflow/sql.ts"; // sql.listen
+
+const listener = sql.listen("yolo", (...argz: unknown[]) =>
+  console.log("yolo", argz),
+);
+console.log("LISTENER", listener);
 
 Deno.serve(async (_req) => {
-  EdgeRuntime.waitUntil(startWorkers(1, (m) => console.log("HANDLER", m)));
+  // EdgeRuntime.waitUntil(startWorkers(1, (m) => console.log("HANDLER", m)));
+  const result = await sql`SELECT now() as time;`;
+  console.log("result");
 
   return new Response(JSON.stringify("ok"), {
     headers: { "Content-Type": "application/json" },
