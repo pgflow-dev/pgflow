4d104717452752181ddd0601f102973dde95c495
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-01 22:28:35 +0100

    chore: remove supabase-worker import

--- pkgs/supabase-worker/README.md
+++
@@ -1,7 +0,0 @@
-# supabase-worker
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test supabase-worker` to execute the unit tests
--- pkgs/supabase-worker/src/supabase-worker.test.ts
+++
@@ -1,6 +0,0 @@
-import { supabaseWorker } from './supabase-worker.ts';
-import { assertEquals } from 'https://deno.land/std@0.172.0/testing/asserts.ts';
-
-Deno.test('should return "supabase-worker"', () => {
-  assertEquals(supabaseWorker(), 'supabase-worker');
-});
