15230692ba82a70db076df1346711922ff389398
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-06 17:36:55 +0100

    chore(supaworker): remove all old files

--- pkgs/supaworker/src/supaworker.test.ts
+++
@@ -1,6 +0,0 @@
-import { supabaseWorker } from './supaworker.ts';
-import { assertEquals } from 'https://deno.land/std@0.172.0/testing/asserts.ts';
-
-Deno.test('should return "supaworker"', () => {
-  assertEquals(supabaseWorker(), 'supaworker');
-});
--- pkgs/supaworker/src/supaworker.ts
+++
@@ -1,7 +0,0 @@
-/**
- * JSDoc for this function
- * @return {string}
- */
-export function supabaseWorker() {
-  return 'supaworker';
-}
