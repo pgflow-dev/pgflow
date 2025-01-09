649ee8bbd0dcff3b8f8efd62a8c617b0223de82f
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-09 14:20:17 +0100

    chore(secrets): Update Supabase secret configurations
    Update app_url port and add Supabase anonymous key secret

--- pkgs/supaworker/supabase/seed.sql
+++ pkgs/supaworker/supabase/seed.sql
@@ -1,1 +1,2 @@
-select vault.create_secret('http://host.docker.internal:54321', 'app_url');
+select vault.create_secret('http://host.docker.internal:50321', 'app_url');
+select vault.create_secret('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0', 'supabase_anon_key');
