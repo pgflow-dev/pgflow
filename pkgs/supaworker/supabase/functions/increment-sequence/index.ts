691a03c1e7338704b714c84f20d9547048bb8fb1
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-09 23:55:25 +0100

    refactor(sequence): simplify sequence increment function
    Streamline sequence creation and logging by removing unnecessary console logs
    and simplifying the incrementCounter function implementation

--- pkgs/supaworker/supabase/functions/increment-sequence/index.ts
+++ pkgs/supaworker/supabase/functions/increment-sequence/index.ts
@@ -3,22 +3,12 @@ import postgres from 'postgres';
 
 const DB_POOL_URL = Deno.env.get('DB_POOL_URL')!;
 console.log('DB_POOL_URL', DB_POOL_URL);
-const sql = postgres(DB_POOL_URL);
-const results = await sql`CREATE SEQUENCE IF NOT EXISTS test_seq`;
-console.log('results', results);
 
-async function incrementCounter(message: any) {
-  console.log('message', message);
-  try {
-    const [{ next_val }] = await sql`
-      SELECT nextval('test_seq') as next_val
-    `;
-    console.log(`Sequence progressed to: ${next_val}`);
-    return next_val;
-  } catch (error) {
-    console.error('Error progressing sequence:', error);
-    throw error;
-  }
+const sql = postgres(DB_POOL_URL);
+await sql`CREATE SEQUENCE IF NOT EXISTS test_seq`;
+
+async function incrementCounter() {
+  console.log('next_seq value', await sql`SELECT nextval('test_seq')`);
 }
 
 Supaworker.start(incrementCounter);
