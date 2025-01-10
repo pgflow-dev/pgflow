96bb9a9990ac9332ab9eb22f571b2833c13639be
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-10 15:02:19 +0100

    fix(sql): silence postgres notices by adding onnotice handler
    Suppress unnecessary database notices during testing by adding a no-op handler to the postgres
    connection configuration

--- pkgs/supaworker/tests/sql.ts
+++ pkgs/supaworker/tests/sql.ts
@@ -1,5 +1,9 @@
 import postgres from 'postgres';
 
-const sql = postgres(Deno.env.get('DB_URL')!);
+const sql = postgres(Deno.env.get('DB_URL')!, {
+  onnotice(_) {
+    // no-op to silence notices
+  },
+});
 
 export { type postgres, sql };
