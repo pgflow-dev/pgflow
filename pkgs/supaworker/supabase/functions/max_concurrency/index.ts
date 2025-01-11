c75fa9ddf80d97ce0ea081568735a9ec44ac6131
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-12 00:42:20 +0100

    feat(supaworker): Add random delay to sequence increment function
    Introduces a random delay mechanism to the sequence increment process
    using the std/async delay utility, which helps simulate variable
    processing times and potential concurrency scenarios

--- pkgs/supaworker/supabase/functions/max_concurrency/index.ts
+++ pkgs/supaworker/supabase/functions/max_concurrency/index.ts
@@ -1,5 +1,6 @@
 import { Supaworker } from '../_supaworker/index.ts';
 import postgres from 'postgres';
+import { delay } from 'jsr:@std/async';
 
 const DB_POOL_URL = Deno.env.get('DB_POOL_URL')!;
 console.log('DB_POOL_URL', DB_POOL_URL);
@@ -9,6 +10,9 @@ await sql`SELECT pgmq.create('max_concurrency')`;
 await sql`CREATE SEQUENCE IF NOT EXISTS test_seq`;
 
 async function incrementSeq() {
+  const randTimeMs = Math.floor(Math.random() * 1000);
+  await delay(randTimeMs);
+
   console.log('last_val is ', await sql`SELECT nextval('test_seq')`);
 }
 
