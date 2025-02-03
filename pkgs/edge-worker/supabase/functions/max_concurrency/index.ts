0d84999e8ba656980f17eb3efb8f0a4b20f01be0
Wojtek Majewski <wojciech.majewski@pm.me>
2025-02-03 14:21:27 +0100

    refactor(edge-worker): simplify delay mechanism in max concurrency function
    Remove random delay and replace with minimal delay to optimize function behavior

--- pkgs/edge-worker/supabase/functions/max_concurrency/index.ts
+++ pkgs/edge-worker/supabase/functions/max_concurrency/index.ts
@@ -8,9 +8,9 @@ console.log('EDGE_WORKER_DB_URL', EDGE_WORKER_DB_URL);
 const sql = postgres(EDGE_WORKER_DB_URL, { prepare: false });
 
 async function incrementSeq() {
-  // await delay(1000);
-  const randTimeMs = Math.floor(Math.random() * 100 + 50);
-  await delay(randTimeMs);
+  await delay(0);
+  // const randTimeMs = Math.floor(Math.random() * 10 + 5);
+  // await delay(randTimeMs);
   console.log(
     '[max_concurrency] last_val =',
     await sql`SELECT nextval('test_seq')`
