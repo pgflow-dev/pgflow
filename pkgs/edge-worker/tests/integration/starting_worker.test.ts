dbf96f932b4548ca4b8883817da2cc1b33bfbd21
Wojtek Majewski <wojciech.majewski@pm.me>
2025-02-10 19:06:58 +0100

    test(worker): Update integration test for worker startup
    Modify test to include worker initialization, database connection,
    and cleanup.
    Update connection string and add delay for async
    worker startup verification.

--- pkgs/edge-worker/tests/integration/starting_worker.test.ts
+++ pkgs/edge-worker/tests/integration/starting_worker.test.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
 import { Worker } from '../../src/Worker.ts';
+import { delay } from "@std/async/delay";
 
-const DB_URL = 'postgresql://postgres:postgres@localhost:5432/postgres';
+const DB_URL = 'postgresql://supabase_admin:postgres@localhost:5432/postgres';
 
 export function createSql() {
   return postgres(DB_URL, {
@@ -13,14 +14,29 @@ export function createSql() {
 }
 
 Deno.test('Starting worker', async () => {
-  let sql = createSql();
+  const sql = createSql();
+  await sql`delete from edge_worker.workers`;
+
+  const worker = new Worker(console.log, {
+    connectionString: DB_URL,
+    maxPollSeconds: 1
+  });
+
+  worker.startOnlyOnce({
+    edgeFunctionName: 'test',
+    // random uuid
+    workerId: '12345678-1234-1234-1234-123456789012'
+  });
+
+  await delay(100);
 
   try {
-    const dbTime = await sql`select * from edge_worker.workers`;
+    const workers = await sql`select * from edge_worker.workers`;
 
-    console.log(dbTime);
+    console.log(workers);
   }
   finally {
     await sql.end();
+    await worker.stop();
   }
 });
