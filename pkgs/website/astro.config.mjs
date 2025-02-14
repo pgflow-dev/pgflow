19e169af412a843027577f252593605a3f19871f
Wojtek Majewski <wojciech.majewski@pm.me>
2025-02-14 06:09:19 +0100

    feat(docs): Add astro-robots-txt

--- pkgs/website/astro.config.mjs
+++ pkgs/website/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
 import starlightSidebarTopics from 'starlight-sidebar-topics';
+import robotsTxt from 'astro-robots-txt';
 
 const GITHUB_REPO_URL = 'https://github.com/pgflow-dev/pgflow';
 const PLAUSIBLE_PROXY = {
@@ -24,87 +25,85 @@ export default defineConfig({
     '/edge-worker/how-to/run-on-hosted-supabase':
       '/edge-worker/how-to/deploy-to-supabasecom/',
   },
-  integrations: [
-    starlight({
-      favicon: '/favicons/favicon.ico',
-      head: [
-        {
-          tag: 'script',
-          attrs: {
-            defer: true,
-            'data-domain': DOMAIN_NAME,
-            'data-api': PLAUSIBLE_PROXY.url + PLAUSIBLE_PROXY.eventPath,
-            src: PLAUSIBLE_PROXY.url + PLAUSIBLE_PROXY.scriptPath,
-          },
+  integrations: [starlight({
+    favicon: '/favicons/favicon.ico',
+    head: [
+      {
+        tag: 'script',
+        attrs: {
+          defer: true,
+          'data-domain': DOMAIN_NAME,
+          'data-api': PLAUSIBLE_PROXY.url + PLAUSIBLE_PROXY.eventPath,
+          src: PLAUSIBLE_PROXY.url + PLAUSIBLE_PROXY.scriptPath,
         },
-      ],
-      plugins: [
-        starlightLinksValidator(),
-        starlightSidebarTopics([
-          {
-            label: 'Edge Worker',
-            icon: 'open-book',
-            link: '/edge-worker/how-it-works/',
-            id: 'edge-worker',
-            items: [
-              { label: 'How it works?', link: '/edge-worker/how-it-works/' },
-              {
-                label: 'Getting started',
-                autogenerate: { directory: 'edge-worker/getting-started/' },
-              },
-              {
-                label: 'How to?',
-                autogenerate: { directory: 'edge-worker/how-to/' },
-              },
-              {
-                label: 'Project Status',
-                badge: { text: 'important', variant: 'caution' },
-                link: '/edge-worker/project-status/',
-              },
-            ],
-          },
-          {
-            label: 'pgflow',
-            icon: 'open-book',
-            link: '/pgflow/',
-            badge: {
-              text: 'soon!',
-              variant: 'note',
+      },
+    ],
+    plugins: [
+      starlightLinksValidator(),
+      starlightSidebarTopics([
+        {
+          label: 'Edge Worker',
+          icon: 'open-book',
+          link: '/edge-worker/how-it-works/',
+          id: 'edge-worker',
+          items: [
+            { label: 'How it works?', link: '/edge-worker/how-it-works/' },
+            {
+              label: 'Getting started',
+              autogenerate: { directory: 'edge-worker/getting-started/' },
             },
-            items: [
-              { label: 'Getting started', link: '/pgflow/getting-started/' },
-              {
-                label: '⚠️ Project Status',
-                link: '/pgflow/project-status/',
-              },
-            ],
+            {
+              label: 'How to?',
+              autogenerate: { directory: 'edge-worker/how-to/' },
+            },
+            {
+              label: 'Project Status',
+              badge: { text: 'important', variant: 'caution' },
+              link: '/edge-worker/project-status/',
+            },
+          ],
+        },
+        {
+          label: 'pgflow',
+          icon: 'open-book',
+          link: '/pgflow/',
+          badge: {
+            text: 'soon!',
+            variant: 'note',
           },
-          {
-            label: 'Found a bug?',
-            icon: 'github',
-            link: 'https://github.com/pgflow-dev/pgflow/issues/new',
-          },
-        ]),
-      ],
-      title: 'pgflow (Workflow Engine for Supabase Background Tasks and Queues)',
-      description: "A workflow engine for Postgres using Supabase queues and background tasks to process jobs in parallel. Simple and built for a great developer experience.",
-      logo: {
-        replacesTitle: true,
-        light: './src/assets/pgflow-logo-light.svg',
-        dark: './src/assets/pgflow-logo-dark.svg',
-      },
-      customCss: ['./src/styles/global.css'],
-      editLink: {
-        baseUrl: `${GITHUB_REPO_URL}/edit/main/pkgs/website/`,
-      },
-      social: {
-        github: GITHUB_REPO_URL,
-        twitter: 'https://x.com/pgflow_dev',
-        blueSky: 'https://bsky.app/profile/pgflow.bsky.social',
-      },
-      components: {
-        Hero: './src/components/ConditionalHero.astro',
-      },
-    }),
-  ],
+          items: [
+            { label: 'Getting started', link: '/pgflow/getting-started/' },
+            {
+              label: '⚠️ Project Status',
+              link: '/pgflow/project-status/',
+            },
+          ],
+        },
+        {
+          label: 'Found a bug?',
+          icon: 'github',
+          link: 'https://github.com/pgflow-dev/pgflow/issues/new',
+        },
+      ]),
+    ],
+    title: 'pgflow (Workflow Engine for Supabase Background Tasks and Queues)',
+    description: "A workflow engine for Postgres using Supabase queues and background tasks to process jobs in parallel. Simple and built for a great developer experience.",
+    logo: {
+      replacesTitle: true,
+      light: './src/assets/pgflow-logo-light.svg',
+      dark: './src/assets/pgflow-logo-dark.svg',
+    },
+    customCss: ['./src/styles/global.css'],
+    editLink: {
+      baseUrl: `${GITHUB_REPO_URL}/edit/main/pkgs/website/`,
+    },
+    social: {
+      github: GITHUB_REPO_URL,
+      twitter: 'https://x.com/pgflow_dev',
+      blueSky: 'https://bsky.app/profile/pgflow.bsky.social',
+    },
+    components: {
+      Hero: './src/components/ConditionalHero.astro',
+    },
+  }), robotsTxt()],
 });
