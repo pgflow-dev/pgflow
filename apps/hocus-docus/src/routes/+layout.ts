84d6d7b324a2ca6784b6e2d7379ad3ce54f6468d
Wojtek Majewski <wojciech.majewski@pm.me>
2025-02-24 20:38:34 +0100

    refactor(config): update Supabase and environment configuration
    Remove redundant type definitions and simplify Supabase client initialization by directly
    importing environment variables

--- apps/hocus-docus/src/routes/+layout.ts
+++ apps/hocus-docus/src/routes/+layout.ts
@@ -1,44 +1,27 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_SUPABASE_URL: string;
-	readonly PUBLIC_SUPABASE_ANON_KEY: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
 import { createBrowserClient, createServerClient, isBrowser, parse } from '@supabase/ssr';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const load: LayoutLoad = async ({ data, depends, fetch }) => {
 	depends('supabase:auth');
 
 	const supabase = isBrowser()
-		? createBrowserClient(
-				import.meta.env.PUBLIC_SUPABASE_URL,
-				import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-				{
-					global: { fetch },
-					cookies: {
-						get(key) {
-							const cookie = parse(document.cookie);
-							return cookie[key];
-						}
+		? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: { fetch },
+				cookies: {
+					get(key) {
+						const cookie = parse(document.cookie);
+						return cookie[key];
 					}
 				}
-			)
-		: createServerClient(
-				import.meta.env.PUBLIC_SUPABASE_URL,
-				import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-				{
-					global: { fetch },
-					cookies: {
-						get() {
-							return JSON.stringify(data.session);
-						}
+			})
+		: createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: { fetch },
+				cookies: {
+					get() {
+						return JSON.stringify(data.session);
 					}
 				}
-			);
+			});
 
 	const {
 		data: { session }
