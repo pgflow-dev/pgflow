87b89939eb2d38edd5a2c3ebe86ce00f5f0dc65a
Wojtek Majewski <wojciech.majewski@pm.me>
2025-03-12 11:53:57 +0100

    refactor(sql): Update task flow query with multiple task completions
    Modify two_roots.sql to include additional task polling and completion steps, updating task
    result format and adding sequential task processing

--- pkgs/core/queries/start_flow/two_roots.sql
+++ pkgs/core/queries/start_flow/two_roots.sql
@@ -15,17 +15,33 @@ SELECT pgflow.complete_task(
   (SELECT run_id FROM pgflow.runs LIMIT 1),
   'first',
   0,
-  '{"result": "first completed"}'::jsonb
+  '"first completed"'::jsonb
 );
 SELECT * FROM pgflow.step_states;
 SELECT * FROM pgflow.step_tasks;
--- SELECT * FROM pgmq.q_temp;
---
---
--- -- SELECT array_agg(message->>'step_slug') FROM pgmq.q_temp;
--- SELECT
---     array_agg(DISTINCT message->>'flow_slug'),
--- array_agg(DISTINCT message->>'step_slug')
--- FROM pgmq.q_temp;
+
+SELECT * FROM pgflow.poll_for_tasks('sequential', 1, 1);
+
+SELECT pgflow.complete_task(
+  (SELECT run_id FROM pgflow.runs LIMIT 1),
+  'second',
+  0,
+  '"second completed"'::jsonb
+);
+SELECT * FROM pgflow.step_states;
+SELECT * FROM pgflow.step_tasks;
+
+SELECT * FROM pgflow.poll_for_tasks('sequential', 1, 1);
+
+SELECT pgflow.complete_task(
+  (SELECT run_id FROM pgflow.runs LIMIT 1),
+  'last',
+  0,
+  '"last completed"'::jsonb
+);
+SELECT * FROM pgflow.step_states;
+SELECT * FROM pgflow.step_tasks;
+
+SELECT * FROM pgflow.runs;
 
 ROLLBACK;
