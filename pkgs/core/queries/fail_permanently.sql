e1074c1365a51bfd87db4c30aed1f29e0fb33128
Wojtek Majewski <wojciech.majewski@pm.me>
2025-03-14 16:40:28 +0100

    test(pgflow): Adjust test sleep duration and add coverage queries
    Modify test query sleep time from 1100 to 2 seconds and add multiple
    coverage select statements for different pgflow-related tables

--- pkgs/core/queries/fail_permanently.sql
+++ pkgs/core/queries/fail_permanently.sql
@@ -7,12 +7,12 @@ select pgflow.start_flow('sequential', '{"test": true}'::JSONB);
 
 -- default retry_limit is 1, so failing twice should mark the task as failed
 select poll_and_fail('sequential');
-select pg_sleep(1100);
+select pg_sleep(2);
 select poll_and_fail('sequential');
 select * from pgflow.step_tasks;
 select * from pgmq.q_sequential;
 -- select * from pgflow.step_tasks;
 --
--- select * from pgflow.step_tasks;
--- select * from pgflow.step_states;
--- select * from pgflow.runs;
+select * from pgflow.step_tasks;
+select * from pgflow.step_states;
+select * from pgflow.runs;
