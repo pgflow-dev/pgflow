61174498ade16a6d7cabc00ed4cd10ae438b1931
Wojtek Majewski <wojciech.majewski@pm.me>
2025-04-03 11:59:59 +0200

    chore(build): Add vite-tsconfig-paths plugin to Vite configurations
    Add vite-tsconfig-paths plugin to Vite configuration files across multiple packages to improve
    module resolution and import handling

--- pkgs/dsl/vite.config.ts
+++ pkgs/dsl/vite.config.ts
@@ -2,11 +2,13 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/pkgs/dsl',
   plugins: [
+    tsconfigPaths(),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
