badcb0833cc405f5282e361f2f9a08390d05bf8c
Wojtek Majewski <wojciech.majewski@pm.me>
2025-04-11 15:20:45 +0200

    chore(tests): add eslint disable comment for empty function
    Suppress eslint no-empty-function warning in test utilities

--- pkgs/edge-worker/tests/fakes.ts
+++ pkgs/edge-worker/tests/fakes.ts
@@ -1,5 +1,6 @@
 import type { CreateLoggerFn, Logger } from '../src/platform/types.js';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {};
 
 export const fakeLogger: Logger = {
