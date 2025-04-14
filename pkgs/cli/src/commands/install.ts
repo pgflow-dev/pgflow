1764b300099d1771ad852f775414783c7384153a
Wojtek Majewski <wojciech.majewski@pm.me>
2025-04-14 17:59:34 +0200

    feat(cli): enhance install command with improved validation and logging
    Add path validation function for Supabase project directory
    Update install command with more descriptive intro and logging steps
    Improve user experience by adding initial value and more robust validation

--- pkgs/cli/src/commands/install.ts
+++ pkgs/cli/src/commands/install.ts
@@ -1,28 +1,37 @@
 import fs from 'fs';
 import { type Command } from 'commander';
-import { intro, isCancel, text, log } from '@clack/prompts';
+import { intro, isCancel, text, log, note } from '@clack/prompts';
+
+function validate(path: string) {
+  const pathsToTest = [
+    [path, 'is not a valid path'],
+    [`${path}/config.toml`, 'does not contain config.toml'],
+    [`${path}/migrations`, 'does not contain migrations folder!'],
+  ];
+
+  // if any of the pathsToTest fail, return the error message
+  for (const [testPath, errorMessage] of pathsToTest) {
+    if (!fs.existsSync(testPath)) {
+      return `${path} ${errorMessage}`;
+    }
+  }
+
+  // otherwise, return undefined
+  return undefined;
+}
 
 export default (program: Command) => {
   program
     .command('install')
     .description('Installs pgflow migration and worker edge function')
-    // .option(
-    //   '--supabase-path <supabase-path>',
-    //   'Path to supabase project',
-    //   false
-    // )
     .action(async () => {
-      intro('pgflow - installing the migrations');
+      intro('pgflow - Postgres-native workflows for Supabase');
 
       const result = await text({
         message: 'Enter the path to your supabase/ directory',
-        placeholder: './supabase/',
-        validate(value) {
-          if (!fs.existsSync(value)) {
-            return `${value} is not a valid path`;
-          }
-          return undefined;
-        },
+        placeholder: 'supabase/',
+        initialValue: 'supabase/',
+        validate,
       });
 
       if (isCancel(result)) {
@@ -30,6 +39,8 @@ export default (program: Command) => {
         return;
       }
 
-      log.success(`Installing pgflow into ${result}`);
+      log.info(`Copying migrations`);
+      log.info(`Updating config.toml`);
+      log.info(`Restarting Supabase`);
     });
 };
