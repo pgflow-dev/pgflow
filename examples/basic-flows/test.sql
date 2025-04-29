fada6d0e0c92203150d49e9c74ffe4dce389888e
Wojtek Majewski <wojciech.majewski@pm.me>
2025-04-29 05:38:27 +0200

    chore: update SQL test data and increase flow execution count
    Expanded test SQL with additional queries and comments, and increased the number of flow starts
    from 1,000 to 10,000 for more extensive testing.
    Removed redundant or commented-out code to improve clarity and maintainability.

--- examples/basic-flows/test.sql
+++ examples/basic-flows/test.sql
@@ -1,3 +1,23 @@
+-- count of messages, by attempts count
+select
+  read_ct as "attempts count",
+  count(*) as "total messsages"
+from pgmq.q_sequential
+group by read_ct
+;
+
+-- tasks with input and output
+select
+  step_slug,
+  pgflow.step_tasks.output as step_output,
+  pgflow.runs.input as run_input,
+  pgflow.runs.output as run_output
+from pgflow.step_tasks
+  left join pgflow.runs on pgflow.step_tasks.run_id = pgflow.runs.run_id
+order by message_id;
+
+-- count of all workers (active and inactive together)
+select count(*) from pgflow.workers;
 
 /*
 
@@ -9,16 +29,6 @@ delete from pgflow.step_states;
 delete from pgflow.runs;
 
 -- start flow
-select pgflow.start_flow('sequential', i::text::jsonb) from generate_series(1, 1000) as i;
+select pgflow.start_flow('sequential', i::text::jsonb) from generate_series(1, 10000) as i;
 
 */
-
-select
-  step_slug,
-  pgflow.step_tasks.output as step_output,
-  pgflow.runs.input as run_input,
-  pgflow.runs.output as run_output
-from pgflow.step_tasks
-  left join pgflow.runs on pgflow.step_tasks.run_id = pgflow.runs.run_id
-order by message_id;
-
